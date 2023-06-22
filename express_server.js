6f6a4a040e7e65aefd7f4c9285600b5b18e24eb5
carolineolagunju <olamidejr21@gmail.com>
2023-06-22 15:57:32 -0600

    clean code

--- express_server.js
+++ express_server.js
@@ -8,16 +8,31 @@ const urlDatabase = {
   "9sm5xk": "http://www.google.com"
 };
 
+//hello page
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+//json page
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+//hello world page http://localhost:8080/hello
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n")
+});
+
+//urls page
+app.get("/urls", (req, res) => {
+  const templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars);
+});
+
+//short and long url page
+app.get("/urls/:id", (req, res) => {
+  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
+  res.render("urls_show", templateVars);
 });
 
 app.listen(PORT, () => {
