30f076678582e42a4f9b4822a88bc3793ad2e3fb
carolineolagunju <olamidejr21@gmail.com>
2023-06-22 19:27:46 -0600

    implement function generate random string & implement post route

--- express_server.js
+++ express_server.js
@@ -2,10 +2,21 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 app.set("view engine", "ejs");
+app.use(express.urlencoded({extended:true}));''
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xk": "http://www.google.com"
+};
+
+const generateRandomString = function() {
+  let output = "";
+  const string = "abcdefghijklmnopqrstuvwxyz0123456789";
+
+  for (let i = 0; i < 6; i++) {
+    output += string.charAt(Math.floor(Math.random() * string.length));
+  }
+  return output;
 };
 
 //hello page
@@ -29,12 +40,22 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+//the form page
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 //short and long url page
 app.get("/urls/:id", (req, res) => {
   const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
 
+//post route
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('Ok');
+})
 app.listen(PORT, () => {
   console.log(`Example app listening on PORT ${PORT}!`);
 });
