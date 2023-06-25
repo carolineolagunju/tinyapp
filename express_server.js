fa3e50ffddf38218efcde11555370fce6a0c1570
carolineolagunju <olamidejr21@gmail.com>
2023-06-25 15:48:03 -0600

    implemented a post route to update a url

--- express_server.js
+++ express_server.js
@@ -4,11 +4,15 @@ const PORT = 8080;
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended:true}));
 
+
+// Database object
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xk": "http://www.google.com"
 };
 
+
+// function to generate a random short url
 const generateRandomString = function() {
   let output = "";
   const string = "abcdefghijklmnopqrstuvwxyz0123456789";
@@ -19,20 +23,24 @@ const generateRandomString = function() {
   return output;
 };
 
+
 //hello page
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
+
 
 //json page
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
 
+
 //hello world page http://localhost:8080/hello
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
 
 //urls page
 app.get("/urls", (req, res) => {
@@ -40,16 +48,19 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+
 //the form page
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
+
 
 //short and long url page
 app.get("/urls/:id", (req, res) => {
   const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
   res.render("urls_show", templateVars);
 });
+
 
 //post route
 app.post("/urls", (req, res) => {
@@ -61,27 +72,35 @@ app.post("/urls", (req, res) => {
 });
 
 
+//post route that updates a url resource
+app.post("/urls/:id", (req, res) => {
+  const urlToEdit = req.params.id;
+  const newURL = req.body.newURL;
+  if(urlDatabase[urlToEdit]);
+  urlDatabase[urlToEdit] = newURL;
+  res.redirect("/urls");
+});
+
+
 //This route deletes a specified url and redirects the page back to /urls
 app.post("/urls/:id/delete", (req, res) => {
-  const urlToDelete = urlDatabase[req.params.id];
-  //loop through the keys of urlDatabase
-  for (const url in urlDatabase) {
-    if (urlDatabase[url] === urlToDelete) {
-      delete urlDatabase[url];
-    } else {
-      res.send(urlDatabase[url]);
-    }
+  const urlToDelete = req.params.id;
+  if (urlDatabase[urlToDelete]) {
+    delete urlDatabase[urlToDelete];
+  } else {
+    res.send(`url not found`);
   }
   res.redirect("/urls");
 });
 
 
-//redirect
+//redirect route
 app.get("/u/:id", (req, res) => {
   const shortId = req.params.id;
   const longURL = urlDatabase[shortId];
   res.redirect(longURL);
 });
+
 
 app.listen(PORT, () => {
   console.log(`Example app listening on PORT ${PORT}!`);
