9a4b3caa095178c1317459c15bd08f53934de6e1
carolineolagunju <olamidejr21@gmail.com>
2023-06-27 22:14:54 -0600

    refactored login route

--- express_server.js
+++ express_server.js
@@ -1,3 +1,4 @@
+// express setup
 const express = require('express');
 const app = express();
 const PORT = 8080;
@@ -7,11 +8,13 @@ const cookieParser = require('cookie-parser');
 app.use(cookieParser());
 
 
+
 // url Database object
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xk": "http://www.google.com"
 };
+
 
 
 //users Database Object
@@ -29,7 +32,8 @@ const users = {
 };
 
 
-// function to generate a random short url
+
+// function to generate a random ucid
 const generateRandomString = function() {
   let output = "";
   const string = "abcdefghijklmnopqrstuvwxyz0123456789";
@@ -40,41 +44,51 @@ const generateRandomString = function() {
 };
 
 
+
+// Function to find a user by email
 const findUser = function(userEmail) {
   let currentUserObj;
 
-  for (const userInfo in users) {
-    if(users[userInfo]["email"] === userEmail) {
-      currentUserObj = users[userInfo];
+  for (const user in users) {
+    if (users[user]["email"] === userEmail) {
+      currentUserObj = users[user];
     }
-  } 
-  return currentUserObj
-}
+  }
+  return currentUserObj;
+};
 
-//urls page
+
+
+//my urls page
 app.get("/urls", (req, res) => {
   const id = req.cookies.user_id;
-  console.log(id);
-  console.log(users);
-  const templateVars = {urls: urlDatabase, email: users[id]["email"]};
+  const user = users[id];
+  const templateVars = {urls: urlDatabase, user};
   res.render("urls_index", templateVars);
 });
 
 
-//the form page
+
+//create new url page
 app.get("/urls/new", (req, res) => {
   const id = req.cookies.user_id;
-  const templateVars = {urls: urlDatabase, email: users[id]["email"]};
+  const user = users[id];
+  const templateVars = {urls: urlDatabase, user};
   res.render("urls_new", templateVars);
 });
 
 
-//short and long url page
+
+//Existing url page
 app.get("/urls/:id", (req, res) => {
-  const id = req.cookies.user_id;
-  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id], email: users[id]["email"]};
+  const userId = req.cookies.user_id;
+  const user = users[userId];
+  const id = req.params.id;
+  const longURL = urlDatabase[id];
+  const templateVars = {id, longURL, user};
   res.render("urls_show", templateVars);
 });
+
 
 
 //post route for creating a new url
@@ -87,19 +101,25 @@ app.post("/urls", (req, res) => {
 });
 
 
+
 //post route that updates a url resource
 app.post("/urls/:id", (req, res) => {
   const urlToEdit = req.params.id;
   const newURL = req.body.newURL;
-  if (urlDatabase[urlToEdit]);
-  urlDatabase[urlToEdit] = newURL;
-  res.redirect("/urls");
+
+  if (urlDatabase[urlToEdit]) {
+    urlDatabase[urlToEdit] = newURL;
+    res.redirect("/urls");
+  }
 });
+
 
 
 //This route deletes a specified url and redirects the page back to /urls
 app.post("/urls/:id/delete", (req, res) => {
   const urlToDelete = req.params.id;
+
+  //Checking if the url to be deleted exist in the urlDatabase
   if (urlDatabase[urlToDelete]) {
     delete urlDatabase[urlToDelete];
   } else {
@@ -107,6 +127,7 @@ app.post("/urls/:id/delete", (req, res) => {
   }
   res.redirect("/urls");
 });
+
 
 
 //redirect route
@@ -117,10 +138,10 @@ app.get("/u/:id", (req, res) => {
 });
 
 
-//user signup route
+
+//post route for newUser signup
 app.get("/register", (req, res) => {
-  const templateVars = {id: req.params.id, longURL: urlDatabase[req.params.id]};
-  res.render("user_register", templateVars)
+  res.render("user_register", {user: null});
 });
 
 
@@ -132,12 +153,14 @@ app.post("/register", (req, res) => {
   const password = req.body.password;
 
   if (!email || !password) {
-     res.status(400).send(`Please enter a valid password or email`)
+    res.status(400).send(`Please enter a valid password or email`);
+    return;
   }
   
-   if (findUser(email)) {
+  if (findUser(email)) {
     res.status(400).send(`email already exists, please enter a valid email`);
-   }
+    return;
+  }
 
   const newUser = {id, email, password};
   users[id] = newUser;
@@ -147,20 +170,35 @@ app.post("/register", (req, res) => {
 
 
 
-//user login route
-app.post("/login", (req, res) => {
-  const newUsername = req.body.email;
-  //sets the username as a cookie
-  res.cookie("username", newUsername);
-  res.redirect("/urls");
+//get route for userlogin
+app.get("/login", (req, res) => {
+  res.render("user_login", {user: null});
 });
+
+
+
+//post route for userlogin
+app.post("/login", (req, res) => {
+  const {email, password} = req.body;
+  const user = findUser(email);
+
+  if (user && password === user.password) {
+    res.cookie("user_id", user.id);
+    res.redirect("/urls");
+    return;
+  }
+
+  res.status(403).send(`User not found, please enter a valid email or password`);
+});
+
 
 
 //user logout route
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
-  res.redirect("/register");
+  res.redirect("/login");
 });
+
 
 
 app.listen(PORT, () => {
