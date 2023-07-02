6a3ee9a490c935b2b667d672c906f032c872b823
carolineolagunju <olamidejr21@gmail.com>
2023-07-01 22:39:41 -0600

    created new module for helper functions

---
+++ test/helpersTest.js
@@ -0,0 +1,28 @@
+const { assert } = require("chai");
+const { getUserByEmail } = require('../helpers');
+
+const testUsers = {
+  "userRandomID": {
+    id: "userRandomID", 
+    email: "user@example.com", 
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID", 
+    email: "user2@example.com", 
+    password: "dishwasher-funk"
+  }
+};
+
+describe('#getUserByEmail', () => {
+  it('should return a user with valid email', () => {
+    const user = getUserByEmail("user@example.com", testUsers);
+    const expectedUserID = "userRandomID";
+    assert.equal(user.id, expectedUserID);
+  });
+
+  it('should return undefined for non existing email', () => {
+    const user = getUserByEmail("pika@picachu.com", testUsers);
+    assert.equal(user, undefined);
+  });
+});
